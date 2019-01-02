96f08c4f902cf123c50f455360cd935a0ae79697
inajob <ina_job@hotmail.com>
2019-01-02 13:58:49 +0900

    add downloader

---
+++ example/downloader/main.lua
@@ -0,0 +1,95 @@
+json = require("/downloader/json.lua")
+
+M_INIT = 0
+M_FETCH = 1
+M_GAMELIST = 2
+M_FETCHGAMEINFO = 3
+M_GAMEINFO = 4
+M_GAMEDL = 5
+
+mode = M_INIT
+gameList = {}
+cursor = 1
+gameName = ""
+gameInfo = {}
+files = {}
+
+function cls()
+    color(0,0,0)
+    fillrect(0,0,128,128) 
+end
+
+function drawStatusText(s)
+  cls()
+  color(0,255,0)
+  text(s, 0, 0)
+end
+
+function fetchGames()
+  collectgarbage("collect")
+  local file = httpsget("raw.githubusercontent.com","/inajob/o-bako-esp32/master/example/games.json")
+  return json.decode(file)
+end
+
+function storeFile(game, fname)
+  collectgarbage("collect")
+  return httpsgetfile("raw.githubusercontent.com","/inajob/o-bako-esp32/master/example/" .. game .. "/" .. fname, "/" .. game .. "/" .. fname)
+end
+
+function fetchFile(game, fname)
+  collectgarbage("collect")
+  return httpsget("raw.githubusercontent.com","/inajob/o-bako-esp32/master/example/" .. game .. "/" .. fname)
+end
+
+function fetchGameInfo(game)
+  local file = fetchFile(game, "game.json")
+  return json.decode(file)
+end
+
+function drawGameList(x, y, games, cursor)
+  for i,v in pairs(games) do
+    text(v, 8 + x, (i - 1) * 8 + y)
+  end 
+  text(">", 0 + x, (cursor - 1) * 8 + y)
+end
+
+function drawGameInfo(x, y, game)
+  text("title:" .. game["title"],x ,y)
+  text("files:",x ,y + 8)
+  for i,v in pairs(game["files"]) do
+    text(v, 8 + x , (i - 1) * 8 + 16 + y)
+  end 
+end
+
+function setup()
+    cls()
+end
+
+function loop()
+  if mode == M_INIT then
+    cls()
+    drawStatusText("fetch Game List ...")
+    mode = M_FETCH
+  elseif mode == M_FETCH then
+    local games = fetchGames()
+    gameList = games["games"]
+    cls()
+    mode = M_GAMELIST
+  elseif mode == M_GAMELIST then
+    cls()
+    color(0,255,0)
+    if btn(2) == 2 and cursor > 1 then
+      cursor = cursor - 1
+    end
+    if btn(3) == 2 and cursor < #gameList then
+      cursor = cursor + 1
+    end
+    if btn(5) == 2 then
+      gameName = gameList[cursor]
+      mode = M_FETCHGAMEINFO
+      cls()
+      drawStatusText("fetch GameInfo " .. gameName .. "...")
+    end
+    drawGameList(0, 16, gameList, cursor)
+  elseif mode == M_FETCHGAMEINFO then
+
