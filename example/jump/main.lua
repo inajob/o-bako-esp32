96e344df0844975b9a30828c2fe19a4eeb5bb2fc
inajob <ina_job@hotmail.com>
2019-01-02 10:56:45 +0900

    update jump game

--- example/jump/main.lua
+++ example/jump/main.lua
@@ -1,140 +1,104 @@
---  launcher
+-- jump game
 
 function setup()
 end
 
 count = 0
-color(0,255,255)
+x = 16
+y = 100
+yv = 0
+jumpflag = false
+enemy = {}
+preventcount = 0
+modetitle = 0
+modegame = 1
+modeover = 2
+mode = modetitle
 
-fs = list()
-cursor = 0
-tabCursor = 0
-
-function drawTab(x, y, w)
-  spr(x, y,8,8,32,0,8,8) -- left
-  for i = 1,w do
-    spr(x + i*8, y,8,8,40,0,8,8) -- middle
-  end
-  spr(x + w*8 ,y,8,8,48,0,8,8) -- right
+function mkenemy(x, y)
+    return {x = x, y = y}
 end
-function drawDisableTab(x, y, w)
-  spr(x, y,8,8,32,8,8,8) -- left
-  for i = 1,w do
-    spr(x + i*8, y,8,8,40,8,8,8) -- middle
-  end
-  spr(x + w*8 ,y,8,8,48,8,8,8) -- right
-end
-
-function drawTile(x, y, w, h)
-  for i=0,h-1 do
-    for j=0,w-1 do
-      spr(x + j*8, y + i*8,8,8,64,0,8,8)
-    end
-  end
-end
+enemy[#enemy] = mkenemy(128, 100);
+test = {x=0, y=0}
 
 function loop()
-   color(0,0,0)
-   fillrect(0, 0, 128, 128)
-   color(255,255,255)
-   if iswifidebug() then
-     text("wifi: on", 0, 0)
-     text(getip(), 50, 0)
-   else
-     text("wifi: off", 0, 0)
-   end
-
-   if btn(1) == 2 then
-     tabCursor = tabCursor + 1
-     if tabCursor > 1 then
-       tabCursor = 0
-     end
-     cursor = 0
-   end
-
-   drawTile(0,18,16,14)
-
-   if tabCursor == 0 then
-     drawFile()
-   else
-     drawUtil()
-   end
-   count = count + 1
+    if mode == modetitle then
+        title()
+    elseif mode == modegame then
+        game()
+    elseif mode == modeover then
+        over()
+    end
 end
 
-function drawFile()
-   drawTab(0,10,4)
-   color(0,0,0)
-   text("file", 8, 11)
+function title()
+    color(0,0,0)
+    fillrect(0,0,128,128)
+    color(255,255,255)
+    text("JUMP", 32, 50)
 
-   drawDisableTab(40,10,4)
-   color(0,0,0)
-   text("util", 48, 11)
-
-   for i, v in pairs(fs) do
-     text(v, 10, 20 + 10 * i)
-   end
-   spr(0, 20 + cursor*10, 8, 8, 32, 16, 8, 8)
-
-   if btn(3) == 2 then
-     cursor = cursor + 1
-     if cursor > #fs then
-       cursor = 0
-     end
-   end
-
-   if btn(4) == 2 then
-     run(fs[cursor])
-   end
+    if btn(3) == 1 then
+        mode = modegame
+        enemy = {}
+    end
 end
 
-utilMenu = {"reload", "wifi on", "self wifi on", "reboot", "tone on", "tone off"}
-function drawUtil()
-   drawDisableTab(0,10,4)
-   color(0,0,0)
-   text("file", 8, 11)
-
-   drawTab(40,10,4)
-   color(0,0,0)
-   text("util", 48, 11)
-
-   for k, v in pairs(utilMenu) do
-     text((k - 1) .. ":" .. v, 10, 20 + 10 * (k - 1))
-   end
-   --fillrect(0, 20 + cursor * 10, 10, 10)
-   spr(0, 20 + cursor*10, 8, 8, 32, 16, 8, 8)
-
-   if btn(3) == 2 then
-     cursor = cursor + 1
-     if cursor >= #utilMenu then
-       cursor = 0
-     end
-   end
-
-   if btn(4) == 2 then
-     if cursor == 0 then
-       run("/init/main.lua")
-     elseif cursor == 1 then
-       if not(iswifidebug()) then
-         wifiserve()
-       end
-     elseif cursor == 2 then
-       if not(iswifidebug()) then
-         wifiserve("ap")
-       end
-     elseif cursor == 3 then
-       reboot()
-     elseif cursor == 4 then
-       tone(0, 523, 0)
-       tone(1, 659, 0)
-       tone(2, 784, 0)
-     elseif cursor == 5 then
-       tone(0, 0)
-       tone(1, 0)
-       tone(2, 0)
-     end
-
-   end
-
+function over()
+    color(255,255,255)
+    text("GAME OVER", 32, 50)
+    if btn(3) == 1 then
+        mode = modetitle
+    end
 end
 
+function game()
+    color(0,0,0)
+    fillrect(0,0,128,128)
+
+    if btn(1) == 1 and not(jumpflag) then
+        yv = -7
+        jumpflag = true
+    end
+    if math.abs(yv) < 8 then
+        yv = yv + 0.3
+    end
+    y = y + yv
+    if y > 100 then
+        y = 100
+        jumpflag = false
+    end
+
+    if math.random(100) == 1 and preventcount == 0 then
+      enemy[#enemy + 1] = mkenemy(128, 100);
+      preventcount = 32
+    end
+
+    -- hit check
+    for i, e in pairs(enemy) do
+        cx = x + 8
+        cy = y + 8
+        cex = e.x + 8
+        cey = e.y + 8
+        if math.abs(cx - cex) < 8 and math.abs(cy - cey) < 8 then
+            mode = modeover
+        end
+    end
+
+    spr(x, math.floor(y), 16, 16, 0, 0)
+    nextenemy = {}
+    for i, e in pairs(enemy) do
+        e.x = e.x - 1
+        if e.x > -16 then
+            nextenemy[#nextenemy + 1] = e
+        end
+        spr(e.x, e.y, 16, 16, 16, 0)
+    end
+    nextenemy = enemy
+    color(100,100,0)
+    fillrect(0,116,128,10)
+
+    count = count + 1
+    if preventcount > 0 then
+        preventcount = preventcount - 1
+    end
+end
